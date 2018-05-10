c227029a0e7a04100e4bdcfdd14186f2be8227c4
abryu082 <wenboyu2208@gmail.com>
2018-05-09 21:26:32 -0400

    update

---
+++ helper.js
@@ -0,0 +1,40 @@
+var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+var fs = require('fs');
+
+
+var visualRecognition = new VisualRecognitionV3({
+
+    version: '2018-03-19',
+    api_key: '40f51d5e0d313b46835129e22896edab51fbf374'
+
+});
+
+/*
+
+var params = {
+    name: 'FaceRecognition',
+    BarackObama_positive_examples: fs.createReadStream('./BarackObama.zip'),
+    DonaldTrump_positive_examples: fs.createReadStream('./DonaldTrump.zip'),
+    WenboYu_positive_examples: fs.createReadStream('./WenboYu.zip'),
+};
+
+visualRecognition.createClassifier(params,
+    function (err, response) {
+        if (err)
+            console.log(err);
+        else
+            console.log(JSON.stringify(response, null, 2))
+    });
+
+*/
+
+visualRecognition.getClassifier({
+        classifier_id: 'FaceRecognition_1498123913'
+    },
+    function (err, response) {
+        if (err)
+            console.log(err);
+        else
+            console.log(JSON.stringify(response, null, 2))
+    }
+);
